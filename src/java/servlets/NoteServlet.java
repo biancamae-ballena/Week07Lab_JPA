3077c2ce945e7081a17080de10efeadc2c829abb
785284 <785284@ELW9IT223353.ACDM.DS.SAIT.CA>
2020-03-26 13:33:12 -0600

    Updated.

--- src/java/servlets/NoteServlet.java
+++ src/java/servlets/NoteServlet.java
@@ -6,11 +6,15 @@
 package servlets;
 
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import models.Note;
+import services.NoteService;
 
 /**
  *
@@ -18,6 +22,22 @@ import javax.servlet.http.HttpServletResponse;
  */
 public class NoteServlet extends HttpServlet {
 
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        NoteService ns = new NoteService();
+        List<Note> noteList = ns.getAll();
+        request.setAttribute("noteList", noteList);
+        getServletContext().getRequestDispatcher("/WEB-INF/notes.jsp").forward(request, response);
+    }
+    
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
      * Handles the HTTP <code>GET</code> method.
@@ -27,10 +47,12 @@ public class NoteServlet extends HttpServlet {
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        getServletContext().getRequestDispatcher("/WEB-INF/notes.jsp").forward(request, response);
+        request.setAttribute("mode", "view");
+        processRequest(request, response);
     }
 
     /**
@@ -44,6 +66,62 @@ public class NoteServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        NoteService ns = new NoteService();
+        List<Note> noteList = ns.getAll();
+        request.setAttribute("noteList", noteList);
+        
+        //Edting the notes.
+        if(request.getParameter("edit") != null) {
+            int editNote = Integer.parseInt(request.getParameter("noteId"));
+            Note note = ns.get(editNote);
+            request.setAttribute("selectedNote", note.getNoteid());
+            request.setAttribute("title", note.getTitle());
+            request.setAttribute("content", note.getContents());
+        }
+        
+        //Saving the note
+        if(request.getParameter("btnSave") != null) {
+            int saveNote = Integer.parseInt(request.getParameter("selectedNote"));
+            
+            String title = request.getParameter("title");
+            String content = request.getParameter("content");
+            
+            try {
+                ns.update(saveNote, title, content);
+            } catch (Exception ex) {
+                Logger.getLogger(NoteServlet.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            
+            request.setAttribute("mode", "view");
+            
+        }
+        
+        //Adding a note.
+        if(request.getParameter("btnAdd") != null) {
+            String title = request.getParameter("title");
+            String content = request.getParameter("content");
+            
+            try {
+                ns.insert(title, content);
+            } catch (Exception ex) {
+                Logger.getLogger(NoteServlet.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            
+            request.setAttribute("mode", "view");
+        } 
+            
+        //Deleting a note
+        if(request.getParameter("btnDelete") != null) {
+            int deleteNote = Integer.parseInt(request.getParameter("selectedNote"));
+            
+            try {
+                ns.delete(deleteNote);
+            } catch (Exception ex) {
+                Logger.getLogger(NoteServlet.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            request.setAttribute("mode", "view");
+        }
+        processRequest(request, response);
 //        getServletContext().getRequestDispatcher("/WEB-INF/notes.jsp").forward(request, response);
     }
 
--- web/WEB-INF/notes.jsp
+++ web/WEB-INF/notes.jsp
@@ -17,13 +17,37 @@
             <tr>
                 <th>Date Created</th>
                 <th>Title</th>
+                <th></th>
             </tr>
+            <c:forEach var="notes" items="${noteList}">
+                <tr>
+                    <td>${notes.datecreated}</td>
+                    <td>${notes.title}</td>
+                    <td>
+                        <form method="POST">
+                            <input type="submit" name="edit" value="Edit"/>
+                            <input type="hidden" name="noteId" value="${notes.noteid}"/>
+                        </form>
+                    </td>
+                </tr>
         </table>
+    <c:if test="${mode == 'view'}">
         <h2><b>Add Note</b></h2>
-        <form method="GET">
+        <form method="POST">
             <input type="text" name="title" placeholder="Title"/><br/>
-            <textarea rows="10" cols="40" placeholder="Type a note"></textarea><br/><br/>
-            <input type="submit" name="btnSubmit" value="Add"/>
+            <textarea name="content" rows="10" cols="40" placeholder="Type a note"></textarea><br/><br/>
+            <input type="submit" name="btnAdd" value="Add"/>
         </form>
+    </c:if>
+    <c:if test="${mode == 'edit'}">
+        <h2><b>Edit Note</b></h2>
+        <form method="POST">
+            <input type="hidden" name="selectedNote" placeholder="Title" value="${selectedNote}"/><br/>
+            <input type="submit" name="btnDelete" value="Delete Note"/><br/>
+            <input type="text" name="title" value="${title}"/><br/>
+            <textarea name="content" rows="10" cols="40" placeholder="Type a note">${content}</textarea><br/><br/>
+            <input type="submit" name="btnSave" value="Save"/>
+        </form>
+    </c:if>
     </body>
 </html>
